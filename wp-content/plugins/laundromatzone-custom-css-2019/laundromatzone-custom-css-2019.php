4704f4dd77384238bb673e09cd9910088c7e9826
shawnnaquin <shawn.naquin@gmail.com>
2018-12-16 16:56:31 -0600

    fix php error

--- wp-content/plugins/laundromatzone-custom-css-2019/laundromatzone-custom-css-2019.php
+++ wp-content/plugins/laundromatzone-custom-css-2019/laundromatzone-custom-css-2019.php
@@ -1,4 +1,5 @@
-/*
+<?php
+ /*
 Plugin Name: Laundromatzone Custom CSS 2019
 Plugin URI: wordpress codex
 Description: widget
@@ -7,7 +8,8 @@ Version: 1
 Author URI: https://wordpress.com
 */
 
-add_action('wp_enqueue_scripts', function(){
-	wp_enqueue_style( 'customizecss', ./custom-css.css' );
+add_action( 'wp_enqueue_scripts', function() {
+	wp_enqueue_style( 'customizecss', './custom-css.css' );
 }, 1000 );
 
+?>
